6fb285653885b95e0e3ca90fa16e4ce360e1b0a5
picollo <lucas.picollo@geekhunter.com.br>
2021-02-03 23:18:39 -0300

    chore(app.js): add cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import router from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
 
