41407f4c62f2f49a278f38008ed206a4d32524ea
picollo <lucas.picollo@geekhunter.com.br>
2021-02-03 23:18:07 -0300

    chore(app.js): add helmet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import helmet from 'helmet';
+
 import router from './routes';
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan('common'));
 
 app.use(router);
