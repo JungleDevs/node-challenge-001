ef7a5fbde02d3c51e64976bc1a1e6f2786f3f544
picollo <lucas.picollo@geekhunter.com.br>
2021-02-03 22:59:23 -0300

    chore(package.json): enable es modules

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require('express')
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('*', (req,res) => {
-  res.send('Hello World!')
-})
+app.get("*", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(process.env.PORT || '8080')
+app.listen(process.env.PORT || "8080");
